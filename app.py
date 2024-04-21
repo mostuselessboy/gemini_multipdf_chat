7c538d4b8e66f9f6617ac5bd27da29d5dc63d93d
Hamza <mostuselessboy@gmail.com>
2024-04-21 07:44:34 +0530

    fix

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_vector_store(chunks):
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question from the context in points by rephrasing it in Hindi Langage, dont provide the answer as it is. GIVE ANSWER ONLY IN POINTS LEAVING A LINE BETWEEN EACH POINTS!, if the answer is not in
+    Answer the question from the context in points by rephrasing it in Hindi only, dont provide the answer as it is. GIVE ANSWER ONLY IN POINTS LEAVING A LINE BETWEEN EACH POINTS!, if the answer is not in
     provided context just say, "Please be more concise with your question🙏", don't provide the wrong answer. \n\n
     Context:\n {context}?\n
     Question: \n{question}\n
@@ -56,7 +56,7 @@ def get_conversational_chain():
 
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
-                                   temperature=0.1,
+                                   temperature=0.6,
                                     safety_settings={
                                     genai.types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: genai.types.HarmBlockThreshold.BLOCK_NONE,
                                     genai.types.HarmCategory.HARM_CATEGORY_HARASSMENT: genai.types.HarmBlockThreshold.BLOCK_NONE,
