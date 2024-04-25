6ba766d777fae3244114df180393d9f685e1260b
Hamza <mostuselessboy@gmail.com>
2024-04-25 07:17:10 +0530

    fx

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_vector_store(chunks):
 def get_conversational_chain():
     prompt_template = """
     Answer the question from the context in points by rephrasing it. GIVE ANSWER ONLY IN POINTS if the answer is not in
-    provided context just say, "Please be more concise with your question🙏", don't provide the wrong answer instead give them a similar question that can be found in the Context! PREFER THAT ANSWER IN CONTEXT THAT COMES FIRST!\n\n
+    provided context just say, "Please be more concise with your question🙏", don't provide the wrong answer! PREFER THAT ANSWER IN CONTEXT THAT COMES FIRST!ANSWER IN A FRIENDLY WAY LIKE A HELPER\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
     Answer:
